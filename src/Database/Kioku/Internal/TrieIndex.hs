fd899a1b88852a6c69bedf34753792817c37c82e
Paul Burns <paul.h.burns@gmail.com>
2019-02-04 12:58:47 -0500

    [ch14265] Import <> for older versions of base

--- src/Database/Kioku/Internal/TrieIndex.hs
+++ src/Database/Kioku/Internal/TrieIndex.hs
@@ -14,6 +14,7 @@ import qualified  Data.ByteString.Char8 as CBS
 import            Data.Foldable
 import            Data.Function
 import            Data.List
+import            Data.Monoid ((<>))
 import qualified  Data.Proxy as P
 import qualified  Data.Vector.Unboxed.Mutable as V
 import qualified  Data.Vector.Algorithms.AmericanFlag as S
