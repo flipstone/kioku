d952a6c00491ad0a27dcf0bdbd8d576d5198240c
David Vollbracht <david.vollbracht@gmail.com>
2019-02-05 14:54:02 -0500

    Makes new query test failures more informative
    Splits out the test cases into separate tests, each with its own
    description and producing better assertion failures.

--- test/QueryTest.hs
+++ test/QueryTest.hs
@@ -1,10 +1,11 @@
 module QueryTest where
 
-import            Control.Monad (void)
-import qualified  Data.ByteString as BS
-import            Data.List ((\\))
+import            Control.Monad (void, when)
+import            Control.DeepSeq (deepseq)
+import qualified  Data.ByteString.Char8 as BS
+import qualified  Data.Set as Set
 import            Test.Tasty (TestTree, testGroup)
-import            Test.Tasty.HUnit (testCase, assertBool)
+import            Test.Tasty.HUnit (Assertion, HasCallStack, testCase, assertFailure)
 
 import            Database.Kioku
 
@@ -22,39 +23,60 @@ test_queries :: TestTree
 test_queries =
   testGroup
     "Queries"
-    [ testCase "keyExactIn finds keys when TrieIndex breaks differently than query input" $ do
-        let test1 = TestCase [ TestData "USNTI", TestData "USNYC" ]
-                             [ TestData "USNYC"]
+    [ testCase "keyExactIn can find a single result" $
+        runQueryTest $
+          QueryTest
+            { queryTestData     = [ TestData "USNYC", TestData "USATL", TestData "USNY" ]
+            , queryTestQuery    = keyExactIn $ testDataKey <$> [ TestData "USATL" ]
+            , queryTestExpected = [ TestData "USATL" ]
+            }
 
-        let test2    = TestCase [ TestData "USATL" ]
-                                [ TestData "USATL" ]
+    , testCase "keyExactIn can find multiple results" $
+        runQueryTest $
+          QueryTest
+            { queryTestData     = [ TestData "USNYC", TestData "USATL", TestData "USNY" ]
+            , queryTestQuery    = keyExactIn [ testDataKey $ TestData "USATL" ]
+            , queryTestExpected = [ TestData "USATL" ]
+            }
 
-            test3 = TestCase [ TestData "USNYC", TestData "USNY" ]
-                             [ TestData "USNYC", TestData "USNY" ]
-
-
-        withKiokuDB defaultKiokuPath $ \db -> do
-          void $ createDataSet "kioku_tests" testData db
-          createIndex "kioku_tests" "kioku_tests.index" testDataKey db
-
-          results1 <- query "kioku_tests.index" (keyExactIn $ testDataKey <$> input test1) db
-          assertBool "Query failed" (equals (expected test1) results1)
-
-          results2 <- query "kioku_tests.index" (keyExactIn $ testDataKey <$> input test2) db
-          assertBool "Query failed" (equals (expected test2) results2)
-
-          results3 <- query "kioku_tests.index" (keyExactIn $ testDataKey <$> input test3) db
-          assertBool "Query failed" (equals (expected test3) results3)
+    , testCase "keyExactIn finds keys when TrieIndex breaks differently than query input" $
+        runQueryTest $
+          QueryTest
+            { queryTestData     = [ TestData "USNYC", TestData "USATL", TestData "USNY" ]
+            , queryTestQuery    = keyExactIn $ testDataKey <$> [ TestData "USNYC", TestData "USNY" ]
+            , queryTestExpected = [ TestData "USNYC", TestData "USNY" ]
+            }
     ]
 
-equals :: Eq a => [a] -> [a] -> Bool
-equals x y = null (x \\ y) && null (y \\ x)
+data QueryTest =
+  QueryTest
+    { queryTestData      :: [TestData]
+    , queryTestQuery     :: KiokuQuery
+    , queryTestExpected  :: [TestData]
+    }
 
-testData :: [TestData]
-testData =
-  [ TestData "USNYC", TestData "USATL", TestData "USNY" ]
+runQueryTest :: QueryTest -> IO ()
+runQueryTest test =
+  withKiokuDB defaultKiokuPath $ \db -> do
+    void $ createDataSet "kioku_tests" (queryTestData test) db
+    createIndex "kioku_tests" "kioku_tests.index" testDataKey db
+    results <- query "kioku_tests.index" (queryTestQuery test) db
+    assertSameData (queryTestExpected test) results
 
-data TestCase =
-  TestCase { input    :: [TestData]
-           , expected :: [TestData]
-           }
+assertSameData :: HasCallStack => [TestData] -> [TestData] -> Assertion
+assertSameData expectedData actualData =
+  when (Set.fromList expectedData /= Set.fromList actualData) $
+    let msg = concat [ "Expected result data: "
+                     , show expectedData
+                     , ", but got: "
+                     , show actualData
+                     ]
+     in -- assertFailure includes its own deepseq, but for some reason we also
+        -- need to have one here. Without it we get a "too many pending signals"
+        -- error, which indicates a segfault occerred. We narrowed down the cause
+        -- to using bytes from the actualData to generate the error message after
+        -- the kioku database has been closed (because kioku goes out of its way to
+        -- not copy bytes whenever necessary). deepseq here forces the message to
+        -- be evaluated before assertFailure is called.
+        deepseq msg (assertFailure msg)
+
