27d4bdb7ce5b3012f0044db2ccae205737841abd
James Brechtel <git@jamesbrechtel.com>
2023-05-17 13:35:32 -0400

    [sc-30089] Moves build to GitHub Actions

---
+++ scripts/lib/run-in-container.sh
@@ -0,0 +1,9 @@
+if [ "$IN_DEV_CONTAINER" ]; then
+  # Already in container, nothing to do
+  :
+else
+  # Script was run from outside the container, re-exec inside the container
+  # with the same arguments
+  docker-compose build
+  exec docker-compose run --rm kioku $0 "$@"
+fi
