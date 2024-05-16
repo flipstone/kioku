9da08330b25cf419a8fb4b1d0a01a0a089bb1ced
David Vollbracht <david@flipstone.com>
2024-05-16 10:11:24 -0500

    Updates to GHC 9.6

--- scripts/lib/run-in-container.sh
+++ scripts/lib/run-in-container.sh
@@ -4,6 +4,6 @@ if [ "$IN_DEV_CONTAINER" ]; then
 else
   # Script was run from outside the container, re-exec inside the container
   # with the same arguments
-  docker-compose build
-  exec docker-compose run --rm kioku $0 "$@"
+  docker compose build
+  exec docker compose run --rm kioku $0 "$@"
 fi
